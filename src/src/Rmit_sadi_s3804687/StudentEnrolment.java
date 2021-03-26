25324a30405713f81579be201fb8e73f5396381c
unknown <datngonguyentien@gmail.com>
2021-03-26 15:46:33 +0700

    make studentEnrollment singleton

--- src/src/Rmit_sadi_s3804687/StudentEnrolment.java
+++ src/src/Rmit_sadi_s3804687/StudentEnrolment.java
@@ -1,5 +1,16 @@
 package Rmit_sadi_s3804687;
 
 public class StudentEnrolment {
+    private static StudentEnrolment stuE = null;
+    private StudentEnrolment() {
+    }
+
+    public static StudentEnrolment getInstance(){
+        if (stuE == null){
+            stuE = new StudentEnrolment();
+        }
+        return stuE;
+    }
+
 
 }
